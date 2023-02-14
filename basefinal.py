c1a7f3c3685093e039cf19bd68785ddfa266fde5
kpshayan <101106754+kpshayan@users.noreply.github.com>
2023-02-15 01:53:05 +0530

    Update basefinal.py

--- basefinal.py
+++ basefinal.py
@@ -1,6 +1,6 @@
 import csv
 import webbrowser
-with open("values.csv", "r") as file:
+with open("../values.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
         for value in row:
