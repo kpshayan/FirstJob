bc5e3d91bfa57a73ef4720d63880e0478745e4c0
kpshayan <101106754+kpshayan@users.noreply.github.com>
2023-02-15 01:57:04 +0530

    Update basefinal.py

--- basefinal.py
+++ basefinal.py
@@ -1,7 +1,11 @@
 import csv
 import webbrowser
+import time
 with open("../values.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
         for value in row:
             webbrowser.open_new_tab(value)
+            
+            
+time.sleep(110)
