285c4e29d6bf20a18b3b182f2f7c225619e62103
kpshayan <101106754+kpshayan@users.noreply.github.com>
2023-02-15 00:31:15 +0530

    Create basefinal.py

---
+++ basefinal.py
@@ -0,0 +1,7 @@
+import csv
+import webbrowser
+with open("values.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        for value in row:
+            webbrowser.open_new_tab(value)
