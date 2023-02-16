5e20f3d123b644e5a1285fa0d2c9db04216b924a
kpshayan <101106754+kpshayan@users.noreply.github.com>
2023-02-17 02:18:43 +0530

    Update basefinal.py

--- basefinal.py
+++ basefinal.py
@@ -2,7 +2,7 @@ import csv
 import webbrowser
 import time
 import base64
-with open("../values.csv", "r") as file:
+with open("values.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
         for value in row:
